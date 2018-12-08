c1f649f54273abc5c96577feadd848fe5f8e7533
vagrant <vagrant@homestead>
2018-12-08 07:40:05 +0000

    管理後臺-用戶列表

---
+++ app/Admin/Controllers/UsersController.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\HasResourceActions;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Show;
+
+class UsersController extends Controller
+{
+    use HasResourceActions;
+
+    /**
+     * Index interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+    public function index(Content $content)
+    {
+        return $content
+            ->header('用戶列表')
+            ->body($this->grid());
+    }
+
+    /**
+     * Show interface.
+     *
+     * @param mixed   $id
+     * @param Content $content
+     * @return Content
+     */
+    public function show($id, Content $content)
+    {
+        return $content
+            ->header('Detail')
+            ->description('description')
+            ->body($this->detail($id));
+    }
+
+    /**
+     * Edit interface.
+     *
+     * @param mixed   $id
+     * @param Content $content
+     * @return Content
+     */
+//    public function edit($id, Content $content)
+//    {
+//        return $content
+//            ->header('Edit')
+//            ->description('description')
+//            ->body($this->form()->edit($id));
+//    }
+
+    /**
+     * Create interface.
+     *
+     * @param Content $content
+     * @return Content
+     */
+//    public function create(Content $content)
+//    {
+//        return $content
+//            ->header('Create')
+//            ->description('description')
+//            ->body($this->form());
+//    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new User);
+
+        $grid->id('ID')->sortable();
+        $grid->name('用戶名');
+        $grid->email('信箱');
+        $grid->email_verified('已驗證信箱')->display(function($value){
+           return $value?'是':'否';
+        });
+        $grid->created_at('註冊時間');
+        $grid->disableCreateButton();
+        $grid->actions(function($actions){
+           $actions->disableView();
+           $actions->disableDelete();
+           $actions->disableEdit();
+        });
+        $grid->tools(function($tools){
+           $tools->batch(function($batch){
+              $batch->disableDelete();
+           });
+        });
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed   $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(User::findOrFail($id));
+
+        $show->id('Id');
+        $show->name('Name');
+        $show->email('Email');
+        $show->password('Password');
+        $show->remember_token('Remember token');
+        $show->email_verified('Email verified');
+        $show->created_at('Created at');
+        $show->updated_at('Updated at');
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+//    protected function form()
+//    {
+//        $form = new Form(new User);
+//
+//        $form->text('name', 'Name');
+//        $form->email('email', 'Email');
+//        $form->password('password', 'Password');
+//        $form->text('remember_token', 'Remember token');
+//        $form->switch('email_verified', 'Email verified');
+//
+//        return $form;
+//    }
+}
