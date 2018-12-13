0965026046765fb7568fbb55a562a3fa78a1e925
vagrant <vagrant@homestead>
2018-12-13 14:11:04 +0000

    封裝業務邏輯代碼(訂單跟購物車)

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -6,32 +6,30 @@ use App\Models\ProductSku;
 use Illuminate\Http\Request;
 use App\Http\Requests\AddCartRequest;
 use App\Models\CartItem;
+use App\Services\CartService;
 class CartController extends Controller
 {
-    public function add(AddCartRequest $request){
-        $user = $request->user();
-        $skuId = $request->input('sku_id');
-        $amount = $request->input('amount');
 
-        if($cart = $user->cartItems()->where('product_sku_id', $skuId)->first()){
-            $cart->update(['amount' => $cart->amount + $amount]);
-        } else {
-            $cart = new CartItem(['amount' => $amount]);
-            $cart->user()->associate($user);
-            $cart->productSku()->associate($skuId);
-            $cart->save();
-        }
+    protected $cartService;
+
+    public function __construct(CartService $cartService)
+    {
+        $this->cartService = $cartService;
+    }
+
+    public function add(AddCartRequest $request){
+        $this->cartService->add($request->input('sku_id'),$request->input('amount'));
         return [];
     }
 
     public function index(Request $request){
-        $cartItems = $request->user()->cartItems()->with(['productSku.product'])->get();
+        $cartItems = $this->cartService->get();
         $addresses = $request->user()->addresses()->orderBy('last_used_at','desc')->get();
         return view('cart.index',['cartItems' => $cartItems, 'addresses' => $addresses]);
     }
 
     public function remove(Request $request,ProductSku $sku){
-        $request->user()->cartItems()->where('product_sku_id', $sku->id)->delete();
+        $this->cartService->remove($sku->id);
         return [];
     }
 }
