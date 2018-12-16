3909a903b5b54e63c0be8a9ebf26a8760b595efe
vagrant <vagrant@homestead>
2018-12-16 06:11:10 +0000

    用戶介面 -申請退款

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ApplyRefundRequest;
 use App\Http\Requests\OrderRequest;
 use App\Http\Requests\Request;
 use App\Jobs\CloseOrder;
@@ -82,6 +83,29 @@ class OrdersController extends Controller
             event(new OrderReviewed($order));
         });
         return redirect()->back();
+    }
+
+    public  function  applyRefund(Order $order,ApplyRefundRequest $request){
+        // 校验订单是否属于当前用户
+        $this->authorize('own', $order);
+        // 判断订单是否已付款
+        if (!$order->paid_at) {
+            throw new InvalidRequestException('該訂單已支付,不可退款');
+        }
+        // 判断订单退款状态是否正确
+        if ($order->refund_status !== Order::REFUND_STATUS_PENDING) {
+            throw new InvalidRequestException('該訂單已經申請過退款');
+        }
+        // 将用户输入的退款理由放到订单的 extra 字段中
+        $extra= $order->extra ?: [];
+        $extra['refund_reason'] = $request->input('reason');
+        // 将订单退款状态改为已申请退款
+        $order->update([
+            'refund_status' => Order::REFUND_STATUS_APPLIED,
+            'extra'         => $extra,
+        ]);
+
+        return $order;
     }
 
 }
