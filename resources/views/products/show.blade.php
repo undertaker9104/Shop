388d354e09bece8d9aa7ef880ba31baef58bedd5
vagrant <vagrant@homestead>
2018-12-29 02:41:02 +0000

    募資商品詳情頁

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -12,12 +12,50 @@
                         </div>
                         <div class="col-sm-7">
                             <div class="title">{{ $product->title }}</div>
-                            <div class="price"><label>價格</label><em>￥</em><span>{{ $product->price }}</span></div>
-                            <div class="sales_and_reviews">
-                                <div class="sold_count">累計銷售 <span class="count">{{ $product->sold_count }}</span></div>
-                                <div class="review_count">累計評價 <span class="count">{{ $product->review_count }}</span></div>
-                                <div class="rating" title="評分 {{ $product->rating }}">評分 <span class="count">{{ str_repeat('★', floor($product->rating)) }}{{ str_repeat('☆', 5 - floor($product->rating)) }}</span></div>
-                            </div>
+                            <!-- 众筹商品模块开始 -->
+                            @if($product->type === \App\Models\Product::TYPE_CROWDFUNDING)
+                                <div class="crowdfunding-info">
+                                    <div class="have-text">以募到</div>
+                                    <div class="total-amount"><span class="symbol">￥</span>{{ $product->crowdfunding->total_amount }}</div>
+                                    <!-- 这里使用了 Bootstrap 的进度条组件 -->
+                                    <div class="progress">
+                                        <div class="progress-bar progress-bar-success progress-bar-striped"
+                                             role="progressbar"
+                                             aria-valuenow="{{ $product->crowdfunding->percent }}"
+                                             aria-valuemin="0"
+                                             aria-valuemax="100"
+                                             style="min-width: 1em; width: {{ min($product->crowdfunding->percent, 100) }}%">
+                                        </div>
+                                    </div>
+                                    <div class="progress-info">
+                                        <span class="current-progress">當前進度：{{ $product->crowdfunding->percent }}%</span>
+                                        <span class="pull-right user-count">{{ $product->crowdfunding->user_count }}名支持者</span>
+                                    </div>
+                                    <!-- 如果众筹状态是众筹中，则输出提示语 -->
+                                    @if ($product->crowdfunding->status === \App\Models\CrowdfundingProduct::STATUS_FUNDING)
+                                        <div>此項目必须在
+                                            <span class="text-red">{{ $product->crowdfunding->end_at->format('Y-m-d H:i:s') }}</span>
+                                            前得到
+                                            <span class="text-red">￥{{ $product->crowdfunding->target_amount }}</span>
+                                            的支持才可成功，
+                                            <!-- Carbon 对象的 diffForHumans() 方法可以计算出与当前时间的相对时间，更人性化 -->
+                                            募資將在<span class="text-red">{{ $product->crowdfunding->end_at->diffForHumans(now()) }}</span>结束！
+                                        </div>
+                                    @endif
+                                </div>
+                        @else
+                            <!-- 原普通商品模块开始 -->
+                                <div class="price"><label>價格</label><em>￥</em><span>{{ $product->price }}</span></div>
+                                <div class="sales_and_reviews">
+                                    <div class="sold_count">累計銷量 <span class="count">{{ $product->sold_count }}</span></div>
+                                    <div class="review_count">累計評價 <span class="count">{{ $product->review_count }}</span></div>
+                                    <div class="rating" title="评分 {{ $product->rating }}">評分
+                                        <span class="count">{{ str_repeat('★', floor($product->rating)) }}{{ str_repeat('☆', 5 - floor($product->rating)) }}</span>
+                                    </div>
+                                </div>
+                                <!-- 原普通商品模块结束 -->
+                        @endif
+                        <!-- 众筹商品模块结束 -->
                             <div class="skus">
                                 <label>選擇</label>
                                 <div class="btn-group" data-toggle="buttons">
@@ -38,9 +76,25 @@
                                 @if($favored)
                                     <button class="btn btn-danger btn-disfavor">取消收藏</button>
                                 @else
-                                <button class="btn btn-success btn-favor">❤ 收藏</button>
+                                    <button class="btn btn-success btn-favor">❤ 收藏</button>
                                 @endif
-                                <button class="btn btn-primary btn-add-to-cart">加入購物車</button>
+                            <!-- 众筹商品下单按钮开始 -->
+                                @if($product->type === \App\Models\Product::TYPE_CROWDFUNDING)
+                                    @if(Auth::check())
+                                        @if($product->crowdfunding->status === \App\Models\CrowdfundingProduct::STATUS_FUNDING)
+                                            <button class="btn btn-primary btn-crowdfunding">參加募資</button>
+                                        @else
+                                            <button class="btn btn-primary disabled">
+                                                {{ \App\Models\CrowdfundingProduct::$statusMap[$product->crowdfunding->status] }}
+                                            </button>
+                                        @endif
+                                    @else
+                                        <a class="btn btn-primary" href="{{ route('login') }}">請先登入</a>
+                                    @endif
+                                @else
+                                    <button class="btn btn-primary btn-add-to-cart">加入購物車</button>
+                            @endif
+                            <!-- 众筹商品下单按钮结束 -->
                             </div>
                         </div>
                     </div>
@@ -159,6 +213,80 @@
                             swal('系統錯誤', '', 'error');
                         }
                     })
+            });
+
+            // 参与众筹 按钮点击事件
+            $('.btn-crowdfunding').click(function () {
+                // 判断是否选中 SKU
+                if (!$('label.active input[name=skus]').val()) {
+                    swal('請先選擇商品');
+                    return;
+                }
+                // 把用户的收货地址以 JSON 的形式放入页面，赋值给 addresses 变量
+                var addresses = {!! json_encode(Auth::check() ? Auth::user()->addresses : []) !!};
+                // 使用 jQuery 动态创建一个表单
+                var $form = $('<form class="form-horizontal" role="form"></form>');
+                // 表单中添加一个收货地址的下拉框
+                $form.append('<div class="form-group">' +
+                    '<label class="control-label col-sm-3">選擇地址</label>' +
+                    '<div class="col-sm-9">' +
+                    '<select class="form-control" name="address_id"></select>' +
+                    '</div></div>');
+                // 循环每个收货地址
+                addresses.forEach(function (address) {
+                    // 把当前收货地址添加到收货地址下拉框选项中
+                    $form.find('select[name=address_id]')
+                        .append("<option value='" + address.id + "'>" +
+                            address.full_address + ' ' + address.contact_name + ' ' + address.contact_phone +
+                            '</option>');
+                });
+                // 在表单中添加一个名为 购买数量 的输入框
+                $form.append('<div class="form-group">' +
+                    '<label class="control-label col-sm-3">購買數量</label>' +
+                    '<div class="col-sm-9"><input class="form-control" name="amount">' +
+                    '</div></div>');
+                // 调用 SweetAlert 弹框
+                swal({
+                    text: '參加募資',
+                    content: $form[0], // 弹框的内容就是刚刚创建的表单
+                    buttons: ['取消', '確定']
+                }).then(function (ret) {
+                    // 如果用户没有点确定按钮，则什么也不做
+                    if (!ret) {
+                        return;
+                    }
+                    // 构建请求参数
+                    var req = {
+                        address_id: $form.find('select[name=address_id]').val(),
+                        amount: $form.find('input[name=amount]').val(),
+                        sku_id: $('label.active input[name=skus]').val()
+                    };
+                    // 调用众筹商品下单接口
+                    axios.post('{{ route('crowdfunding_orders.store') }}', req)
+                        .then(function (response) {
+                            // 订单创建成功，跳转到订单详情页
+                            swal('訂單提交成功', '', 'success')
+                                .then(() => {
+                                    location.href = '/orders/' + response.data.id;
+                                });
+                        }, function (error) {
+                            // 输入参数校验失败，展示失败原因
+                            if (error.response.status === 422) {
+                                var html = '<div>';
+                                _.each(error.response.data.errors, function (errors) {
+                                    _.each(errors, function (error) {
+                                        html += error+'<br>';
+                                    })
+                                });
+                                html += '</div>';
+                                swal({content: $(html)[0], icon: 'error'})
+                            } else if (error.response.status === 403) {
+                                swal(error.response.data.msg, '', 'error');
+                            } else {
+                                swal('系統錯誤', '', 'error');
+                            }
+                        });
+                });
             });
         });
     </script>
