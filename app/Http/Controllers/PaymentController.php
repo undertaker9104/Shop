a05593e03c420b4c550091788edafb72bd18eb53
vagrant <vagrant@homestead>
2019-01-02 13:19:40 +0000

    創建分期付款

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\RefundCrowdfundingOrders;
 use Illuminate\Http\Request;
 use App\Models\Order;
 use App\Exceptions\InvalidRequestException;
 use App\Events\OrderPaid;
 use Carbon\Carbon;
 use Omnipay\Omnipay;
-
+use Illuminate\Validation\Rule;
+use App\Models\Installment;
 
 class PaymentController extends Controller
 {
@@ -150,4 +152,50 @@ class PaymentController extends Controller
 
         return app('wechat_pay')->success();
     }
+    public function payByInstallment(Order $order, Request $request) {
+        $this->authorize('own',$order);
+
+        if ($order->paid_at || $order->closed) {
+            throw new InvalidRequestException('訂單狀態不正確');
+        }
+
+        $this->validate($request,[
+           'count' => ['required',Rule::in(array_keys(config('app.installment_fee_rate')))],
+        ]);
+
+        Installment::query()->where('order_id',$order->id)
+                            ->where('status',Installment::STATUS_PENDING)
+                            ->delete();
+        $count = $request->input('count');
+        $installment = new Installment([
+            // 总本金即为商品订单总金额
+            'total_amount' => $order->total_amount,
+            // 分期期数
+            'count'        => $count,
+            // 从配置文件中读取相应期数的费率
+            'fee_rate'     => config('app.installment_fee_rate')[$count],
+            // 从配置文件中读取当期逾期费率
+            'fine_rate'    => config('app.installment_fine_rate'),
+        ]);
+        $installment->user()->associate($request->user());
+        $installment->order()->associate($order);
+        $installment->save();
+        $dueDate = Carbon::tomorrow();
+        $base = big_number($order->total_amount)->divide($count)->getValue();
+        $fee = big_number($base)->multiply($installment->fee_rate)->divide(100)->getValue();
+        for($i = 0; $i < $count; $i++) {
+            if ($i === $count -1) {
+                $base = big_number($order->total_amount)->subtract(big_number($base)->multiply($count - 1));
+            }
+            $installment->items()->create([
+                'sequence' => $i,
+                'base'     => $base,
+                'fee'      => $fee,
+                'due_date' => $dueDate,
+            ]);
+            $dueDate = $dueDate->copy()->addDays(30);
+        }
+        return $installment;
+    }
+
 }
